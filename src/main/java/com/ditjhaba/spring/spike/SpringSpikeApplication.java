66f52aff6ab887dc903f9f3a8717cb27662c8dce
Ditjhaba Selemela <edselemela@gmail.com>
2016-01-19 11:38:33 +0200

    Initial commit

---
+++ src/main/java/com/ditjhaba/spring/spike/SpringSpikeApplication.java
@@ -0,0 +1,12 @@
+package com.ditjhaba.spring.spike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSpikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSpikeApplication.class, args);
+	}
+}
